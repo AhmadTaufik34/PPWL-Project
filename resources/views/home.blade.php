876383de46456ff3fab77759321d7f85ddb092a0
AhmadTaufik34 <trahman339@gmail.com>
2025-11-16 20:44:31 +0700

    Add Admin dashboard with Blade templating

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('title', 'Dashboard')
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+    <div class="row">
+        <!-- Congratulations Card -->
+        <div class="col-lg-8 mb-4 order-0">
+            <div class="card">
+                <div class="d-flex align-items-end row">
+                    <div class="col-sm-7">
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Selamat datang admin website!
+                            </h5>
+                            <p class="mb-4">
+                                selamat bekerja <span class="fw-bold"></span> nikmati harimu
+                                dengan lebih baik
+                            </p>
+                            <a href="javascript:;" class="btn btn-sm btn-outline-primary">Lihat
+                                Data</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 text-center text-sm-left">
+                        <div class="card-body pb-0 px-0 px-md-4">
+                            <img
+                                src="../assets/img/illustrations/man-with-laptop-light.png"
+                                height="140"
+                                alt="View Badge User"
+                                data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                data-app-light-img="illustrations/man-with-laptop-light.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h3 class="card-title text-nowrap mb-1">$4,679</h3>
+        <small class="text-success fw-semibold">
+            <i class="bx bx-up-arrow-alt"></i> +28.42%
+        </small>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 //Admin
 Route::get('/dashboard', function () {
@@ -24,7 +11,17 @@ Route::get('/dashboard', function () {
 
 // User
 Route::get('/', function () {
-    return view('user.home');
+    return view('home');
 })->name('home');
-    
-require __DIR__.'/auth.php';
+
+//route dengan mode resources
+Route::resource('/products', ProductController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__ . '/auth.php';
