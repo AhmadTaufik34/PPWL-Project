40080c7a7a8acbfe2f8c3ee7cf7b2e96d99d10a7
AhmadTaufik34 <trahman339@gmail.com>
2026-01-21 15:14:13 +0700

    Add admin dashboard with Blade templating

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,45 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-//Admin
+// Admin
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'admin'])->name('dashboard');
 
 // User
 Route::get('/', function () {
-    return view('home');
+    $products = Product::latest()->take(8)->get();
+    return view('user.home', compact('products'));
 })->name('home');
-
-//route dengan mode resources
-Route::resource('/products', ProductController::class);
-Route::resource('/categories', CategoryController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
+    //route dengan mode resources
+    Route::resource('/products', ProductController::class);
+    Route::resource('/categories', CategoryController::class);
+
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+    Route::post('/cart/update/{product}', [CartController::class, 'update'])
+    ->name('cart.update');
+    Route::delete('/cart/remove/{product}', [CartController::class, 'remove'])
+    ->name('cart.remove');
+
+    Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
+    Route::post('/checkout', [CheckoutController::class, 'process'])
+    ->name('checkout.process');
+
+});
 
 require __DIR__ . '/auth.php';
